eb7a0aebe1dd4326820c744bbc22a1c63d6f3e3e
MChinchamalatpure <84777572+MChinchamalatpure@users.noreply.github.com>
2021-10-18 11:36:43 +0530

    Update StringCalculatorShould.java
    Added methods for required functionalities

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,59 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    public void string_calculateTwo_expectTwo() {
+        Assert.assertEquals(2, StringCalculator.add("2"));
+    }
+
+    @Test
+    public void string_calculateTwoToOne_expectThree() {
+        Assert.assertEquals(3, StringCalculator.add("2,1"));
+    }
+
+    @Test
+    public void string_calculateTwoToOneWithNewlines_expectThree() {
+        Assert.assertEquals(3, StringCalculator.add("2\n1"));
+    }
+
+    @Test
+    public void string_calculateTwoToOneWithCustomDelimiter_expectThree() {
+        Assert.assertEquals(3, StringCalculator.add("//[x]\n2x1"));
+    }
+
+    @Test
+    public void string_calculateTwoToOneWithCustomDelimiterWithLengthTwo_expectThree() {
+        Assert.assertEquals(3, StringCalculator.add("//[xx]\n2xx1"));
+    }
+
+    @Test
+    public void string_calculateTwoToOneToThreeWithTwoCustomDelimiter_expectThree() {
+        Assert.assertEquals(6, StringCalculator.add("//[x][y]\n2x1y3"));
+    }
+
+    @Test
+    public void string_calculateSingleNegativeValue_exception() {
+        try {
+            StringCalculator.add("-1");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("negatives not allowed (-1)", e.getMessage());
+        }
+    }
+
+    @Test
+    public void string_calculateTwoNegativeValue_exception() {
+        try {
+            StringCalculator.add("-1,-2");
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("negatives not allowed (-1,-2)", e.getMessage());
+        }
+    }
+
+    @Test
+    public void string_calculateTwoToOneThousandOne_Two() {
+        Assert.assertEquals(2, StringCalculator.add("2,1001"));
+    }
 }
